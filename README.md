c6f6694ca37653c8014c64783d8b0c0efd942c21
Mengdi Liu <liumengdi923@gmail.com>
2021-03-11 23:29:07 -0500

    adding new files and modified files to the final submit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # UFOs
-Javascript
+
+## Overview of Project
+### *Purpose*
+The purpose of this analysis is to showcase the research result done by far about Unidentified Flying Objects. The analysis includes an introduction to the community that's supporting the research of the UFO sightings project. Data is displayed in a table with UFO sighting records. The table of the data has columns of Date, City, State, Country, Shape, Duration, and Comments on each record. A search filter is built on the webpage to help readers sort out the information as needed.
+
+It's our job to lay out the discoveries, we believe you will make good use of them and decide for your own where you stand on whether we are alone in the universe, whether the evidence is enough to prove if aliens had attempted to contact us, whether they should be left alone.
+
+### *Tools*
+This analysis uses JavaScript, HTML, and CSS to implement the data representation on a webpage. Bootstrap and jumbotron are two of the libraries used to make the styling of the webpage easier and prettier.
+
+<br/>
+
+## Results
+<br/>
+
+### *The Webpage*
+As mentioned in the overview section, this webpage has provided information for readers and it has different segments to make it user-friendly. Here are all the segments designed for this webpage:
+ * Navigation Bar (UFO Sightings)
+
+ * Page Header (The Truth Is Out There)
+ * Article Title (UFO sightings: Fact or Fancy? Ufologists Weigh In)
+ * Article Paragraph (the paragraph to the right of the title with a brief introduction of the research)
+ * Filters for the Table (filter search with 5 filters to be applied)
+ * Table of Sighting Data (table of data records)
+
+
+<br/>
+
+### *The Search Criteria*
+
+The filter search can be used to modify the data records displayed in the table beside it.
+
+Here are some simple steps to follow:
+* Step 1: Determine the filter criteria for your search
+
+* Step 2: Type in the criteria based on your need following the example input in the blank input box (e.g.search for the shape that's "light", the country being "us", and state being "ca"). The new input criteria will show up as black, whereas the example input will remain grey.
+
+    ![search](images/search.png)
+
+
+* Step 3: When you finished all the input on the search criteria, simply click anywhere else on the webpage
+
+* Step 4: The records matching your search criteria will display in the table
+
+    ![table](images/table.png)
+
+<br/>
+
+## Summary
+<br/>
+
+The webpage does not have a clear way of indicating whether the filter is applied or not. If there are more data records and columns, it could be hard to know whether the filter has been applied or not and therefore cause confusion during a search.
+
+
+Here are two recommendations for further improvement on the webpage. One is to add a button to confirm applying the filter. Another is to add contact information on the top navigation bar in case the reader is interested in getting more inquiries answered.
--- js/app.js
+++ js/app.js
@@ -1,40 +1,77 @@
-// import the data from data.js
+// from data.js
 const tableData = data;
-//reference the HTML table using d3
-var tbody =d3.select("tbody");
 
-function buildTable(data){
-    //clear out any existing data
-    tbody.html("");
-    //loop thru each object in the data
-    //and append a row and cells for each value in the row
-    data.forEach((dataRow)=>{
-        let row = tbody.append("tr");
+// get table references
+var tbody = d3.select("tbody");
 
-        //loop thru each field in the dataRow and add each value as a table cell (td)
-        Object.values(dataRow).forEach((val)=>{
-            let cell= row.append("td");
-            cell.text(val);
-        });
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
+  });
 }
 
-//new function fo handle click and filter
-function handleClick(){
-    let date = d3.select("#datetime").property("value");
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let elementChanged = d3.select(this);
+    // 4b. Save the value that was changed as a variable.
+    let valueChanged = elementChanged.property("value");
+    console.log(valueChanged);
+
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filterId = elementChanged.attr("id");
+    console.log(filterId);
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (valueChanged) {
+      filters[filterId] = valueChanged;
+    }
+    else {
+      delete filters[filterId]
+    }
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+  }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-    //check to see if a date was entered and filter the data using that date.
-    if (date){
-        //apply filter to the table data to only keep the rows where the datetime value matches the filter value
-        filteredData = filteredData.filter(row => row.datetime === date);
-    };
-    //rebuild the table using the filtered data
-    //if no date as entered, then filteredData will just be the original tableData.
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) =>{
+      console.log([key, value]);
+      filteredData = filteredData.filter(row => row[key]===value);
+    });
+
+    // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-}
-
-//attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
-
-//build the table when the page loads
-buildTable(tableData);
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
